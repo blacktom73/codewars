2cf09141d7e8a048d199e52042b3fd0ef5d6c9ba
Tamas Mitro <blacktom73@gmail.com>
2018-02-24 20:42:55 +0100

    Add example to the project

---
+++ src/main/java/hu/mitro/dinglemouse/Dinglemouse.java
@@ -0,0 +1,19 @@
+package main.java.hu.mitro.dinglemouse;
+
+/***
+ * Given number A you must return number B so that (int) (A + B) == 123
+ *
+ * Note B can't be negative
+ *
+ * @author Mitró Tamás
+ *
+ */
+public class Dinglemouse {
+
+	public static long int123(final int a) {
+		int b = 0;
+		b = 123 - a;
+		return b;
+	}
+
+}
--- src/main/java/hu/mitro/paginationhelper/PaginationHelper.java
+++ src/main/java/hu/mitro/paginationhelper/PaginationHelper.java
@@ -4,29 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 /***
+ * For this exercise you will be strengthening your page-fu mastery. You will complete the
+ * PaginationHelper class, which is a utility class helpful for querying paging information related
+ * to an array.
+ *
+ * The class is designed to take in an array of values and an integer indicating how many items will
+ * be allowed per each page. The types of values contained within the collection/array are not
+ * relevant.
+ *
+ * The following are some examples of how this class is used:
  *
  * @author Mitró Tamás
  *
- * @param <I>
- *            For this exercise you will be strengthening your page-fu mastery. You will complete
- *            the PaginationHelper class, which is a utility class helpful for querying paging
- *            information related to an array.
- * 
- *            The class is designed to take in an array of values and an integer indicating how many
- *            items will be allowed per each page. The types of values contained within the
- *            collection/array are not relevant.
- * 
- *            The following are some examples of how this class is used:
- * 
- *            PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c',
- *            'd', 'e', 'f'), 4); helper.pageCount(); //should == 2 helper.itemCount(); //should ==
- *            6 helper.pageItemCount(0); //should == 4 helper.pageItemCount(1); // last page -
- *            should == 2 helper.pageItemCount(2); // should == -1 since the page is invalid
- * 
- *            // pageIndex takes an item index and returns the page that it belongs on
- *            helper.pageIndex(5); //should == 1 (zero based index) helper.pageIndex(2); //should ==
- *            0 helper.pageIndex(20); //should == -1 helper.pageIndex(-10); //should == -1
- * 
+ * @param <I>:
+ *            Type of the elements on the pages
+ *
  */
 public class PaginationHelper<I> {
 
@@ -67,9 +59,9 @@ public class PaginationHelper<I> {
 	 * return -1 for pageIndex values that are out of range
 	 */
 	public int pageItemCount(int pageIndex) {
-		if (pageIndex + 1 < pageCount) {
+		if (pageIndex < pageCount - 1) {
 			return itemsPerPage;
-		} else if (pageIndex + 1 == pageCount) {
+		} else if (pageIndex == pageCount - 1) {
 			return (collection.size() / itemsPerPage) + 1;
 		} else {
 			return -1;
@@ -84,7 +76,7 @@ public class PaginationHelper<I> {
 		if (itemIndex < 0 || itemIndex > collection.size() - 1) {
 			return -1;
 		} else {
-			return itemIndex % itemsPerPage;
+			return itemIndex / itemsPerPage;
 		}
 	}
 
