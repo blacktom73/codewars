5f5d36e25c087a50efb85e3b0b7851cc5b45d23b
Tamas Mitro <blacktom73@gmail.com>
2018-05-22 21:44:30 +0200

    ADD: strongnumber task and solution

---
+++ src/main/java/hu/mitro/strongnumber/StrongNumber.java
@@ -0,0 +1,22 @@
+package main.java.hu.mitro.strongnumber;
+
+public class StrongNumber {
+
+	public static String isStrongNumber(int num) {
+		String number = Integer.toString(num);
+		int[] digits = new int[number.length()];
+		for (int i = 0; i < number.length(); i++) {
+			digits[i] = number.charAt(i) - '0';
+		}
+		int sum = 0;
+		for (int round = 0; round < digits.length; round++) {
+			int factorial = 1;
+			for (int i = 1; i <= digits[round]; i++) {
+				factorial *= i;
+			}
+			sum += factorial;
+		}
+		return sum == num ? "STRONG!!!!" : "Not Strong !!";
+	}
+
+}
---
+++ src/main/java/hu/mitro/strongnumber/StrongNumberTest.java
@@ -0,0 +1,22 @@
+package main.java.hu.mitro.strongnumber;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StrongNumberTest {
+
+	private static String STRONG = "STRONG!!!!";
+	private static String NOT_STRONG = "Not Strong !!";
+
+	@Test
+	public void testSomething() {
+		assertEquals(STRONG, StrongNumber.isStrongNumber(1));
+		assertEquals(STRONG, StrongNumber.isStrongNumber(2));
+		assertEquals(STRONG, StrongNumber.isStrongNumber(145));
+		assertEquals(NOT_STRONG, StrongNumber.isStrongNumber(7));
+		assertEquals(NOT_STRONG, StrongNumber.isStrongNumber(93));
+		assertEquals(NOT_STRONG, StrongNumber.isStrongNumber(185));
+	}
+
+}
