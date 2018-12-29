d8417e9b97adfcf022595778a6801c329ec4419f
Tam�sMitr� <blacktom73@gmail.com>
2018-12-29 21:50:58 +0100

    FIX: FindMaxValueInTree task (not done yet)

--- src/main/java/hu/mitro/findmaxtreenode/FindMaxValueInTree.java
+++ src/main/java/hu/mitro/findmaxtreenode/FindMaxValueInTree.java
@@ -2,48 +2,20 @@ package main.java.hu.mitro.findmaxtreenode;
 
 public class FindMaxValueInTree {
 
-//	static int findMax(TreeNode root) {
-//		int max = root.value;
-//		if (root.left == null && root.right == null) {
-//			return max;
-//		} else {
-//			if(root.left != null){
-//				max = findMax(root.left);
-//				if (root.left.value > max) {
-//					return root.left.value;
-//				} else if (root.right.value > max){
-//					return root.right.value;
-//				}
-//			}
-//			if(root.right != null){
-//				max = findMax(root.right);
-//				if (root.left.value > max) {
-//					return root.left.value;
-//				} else if (root.right.value > max){
-//					return root.right.value;
-//				}
-//			}
-//		}
-//		return max;
-//	}
-
-	static int max;
+	static int max = Integer.MIN_VALUE;
 
 	static int findMax(TreeNode root) {
 		if (root.left == null && root.right == null) {
-			return root.value;
+			return root.value > max ? root.value : max;
 		} else {
-			if(root.left != null){
-				if(root.left.value > root.value){
-					max = root.left.value;
-				}
-				findMax(root.left);
+			if (root.left != null && root.right == null) {
+				max = findMax(root.left);
 			}
-			if(root.right != null){
-				if(root.right.value > root.value){
-					max = root.right.value;
-				}
-				findMax(root.right);
+			if (root.left == null && root.right != null) {
+				max = findMax(root.right);
+			}
+			if (root.left != null && root.right != null) {
+				max = Integer.max(findMax(root.left), findMax(root.right));
 			}
 		}
 		return max;
